75b2a376b8ec65e42f8dc59f4dc3c0baea5d277c
Jean <jeanpj12@gmail.com>
2025-03-25 16:20:56 -0300

    Feat: Add Works and content list  refactored

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,176 +1,153 @@
-import ListiningPost from "@/components/ListiningPosts";
-import { RevealFx } from "@/components/RevealFx";
+import ListiningPost from '@/components/ListiningPosts';
+import { RevealFx } from '@/components/RevealFx';
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Badge } from "@/components/ui/badge";
-import { getPosts } from "@/http/get-posts";
-import { HTTPError } from "ky";
-import Image from "next/image";
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Badge } from '@/components/ui/badge';
+import { getPosts } from '@/http/get-posts';
+import { HTTPError } from 'ky';
+import Image from 'next/image';
+import { experiencies, skills, works } from '@/content';
 
 export default async function Home() {
-  const skills = [
-    "NodeJs",
-    "React",
-    "Next",
-    "TypeScript",
-    "Postgre",
-    "Docker",
-    "Jwt",
-    "SuperTest",
-    "Jest",
-    "Prisma",
-    "Fastify",
-    "Express",
-    "Zod",
-    "Github",
-    "TailwindCSS",
-    "Storybook",
-    "Monólitos",
-    "Microsserviços",
-    "Multi-Tenant",
-  ];
-
-  const experiencies = [
-    {
-      enterprise: "Autônomo",
-      role: "Full Stack Developer",
-      monthFrom: "Jan",
-      yearFrom: "2024",
-      monthTo: " ",
-      yearTo: "Atuando",
-      description:
-        "Atuo como desenvolvedor Full Stack, entregando sites mais completos, robustos e seguro. Além do desenvolvimentos de sistemas também para outras empresas.",
-    },
-    {
-      enterprise: "Group Imobi",
-      role: "Designer Pleno",
-      monthFrom: "Jun",
-      yearFrom: "2024",
-      monthTo: "Out",
-      yearTo: "2024",
-      description:
-        "Desenvolvimento de posts, landing pages e materiais impressos, com participação na criação de estratégias de marketing e otimização de processos. Responsável também pela orientação, treinamento e recrutamento de novos designers.",
-    },
-
-    {
-      enterprise: "STZ Digital",
-      role: "Sócio Proprietário",
-      monthFrom: "Set",
-      yearFrom: "2020",
-      monthTo: "Mai",
-      yearTo: "2024",
-      description:
-        "Como sócio proprietário, tinha participação em todos o setores, inteligência comercial, vendas, design, marketing, desenvolvimento web, e etc. Junto com mais um sócio que veio junto comigo nessa jornada.",
-    },
-    {
-      enterprise: "Group Imobi",
-      role: "Designer Junior",
-      monthFrom: "Jun",
-      yearFrom: "2023",
-      monthTo: "Nov",
-      yearTo: "2023",
-      description:
-        "Desenvolvimento de posts, landing pages e materiais impressos, além de colaboração em estratégias de marketing e otimização de processos.",
-    },
-  ];
-
-  let posts;
-  try {
-    posts = await getPosts();
-  } catch (err) {
-    if (err instanceof HTTPError) {
-      const { message } = await err.response.json();
-      console.log(message);
+    let posts;
+    try {
+        posts = await getPosts();
+    } catch (err) {
+        if (err instanceof HTTPError) {
+            const { message } = await err.response.json();
+            console.log(message);
+        }
     }
-  }
 
-  return (
-    <RevealFx speed="medium">
-      <main className="flex flex-col gap-12 items-center mx-auto md:max-w-[700px]">
-        <div
-          id="hero"
-          className="flex gap-5 md:gap-10 mt-20 items-center box-border"
-        >
-          <div className="flex flex-col gap-4 w-full">
-            <h1 className="text-3xl md:text-5xl font-bold">
-              Olá, eu sou o jean 👋
-            </h1>
-            <span className="text-sm md:text-lg">
-              Full Stack Developer. E esse é o meu blog pessoal, contando sobre
-              minha jornada como dev.
-            </span>
-          </div>
-          <Image
-            src="/avatar.jpg"
-            alt=""
-            width={100}
-            height={100}
-            className="rounded-full md:scale-[1.5]"
-          />
-        </div>
-
-        <div id="posts-wrapper" className="flex flex-col gap-4 w-full">
-          <h2 className="text-xl font-bold">Sobre mim</h2>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-            praesentium est hic eaque consectetur ducimus repellendus porro
-            minus illum culpa ratione quos provident unde, dicta vero excepturi
-            dolores totam corrupti?
-          </span>
-        </div>
-
-        <div id="posts-wrapper" className="flex flex-col gap-4 w-full">
-          <h2 className="text-xl font-bold">Skills</h2>
-          <div className="flex gap-1 flex-wrap">
-            {skills.map((skill) => {
-              return <Badge key={skill}>{skill}</Badge>;
-            })}
-          </div>
-        </div>
-
-        <div id="posts-wrapper" className="flex flex-col gap-4 w-full">
-          <h2 className="text-xl font-bold">Experiências</h2>
-
-          <Accordion type="single" collapsible>
-            {experiencies.map((experiencie, index) => {
-              return (
-                <AccordionItem value={index.toString()} key={index}>
-                  <AccordionTrigger className="flex justify-between">
-                    <div>
-                      <h3 className="text-md">{experiencie.enterprise}</h3>
-                      <span className="text-xs font-normal">
-                        {experiencie.role}
-                      </span>
+    return (
+        <RevealFx speed="medium">
+            <main className="mx-auto flex flex-col items-center gap-12 md:max-w-[700px]">
+                <div
+                    id="hero"
+                    className="mt-20 box-border flex items-center gap-5 md:gap-10"
+                >
+                    <div className="flex w-full flex-col gap-4">
+                        <h1 className="text-3xl font-bold md:text-5xl">
+                            Olá, eu sou o jean 👋
+                        </h1>
+                        <span className="text-sm md:text-lg">
+                            Full Stack Developer. E esse é o meu blog pessoal,
+                            contando sobre minha jornada como dev.
+                        </span>
                     </div>
-                    <div className="font-normal text-sm">
-                      <span>
-                        {experiencie.monthFrom} {experiencie.yearFrom}
-                      </span>
-                      <span> - </span>
-                      <span>
-                        {experiencie.monthTo} {experiencie.yearTo}
-                      </span>
-                    </div>
-                  </AccordionTrigger>
-                  <AccordionContent>{experiencie.description}</AccordionContent>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-        </div>
+                    <Image
+                        src="/avatar.jpg"
+                        alt=""
+                        width={100}
+                        height={100}
+                        className="rounded-full md:scale-[1.5]"
+                    />
+                </div>
 
-        {posts && (
+                <div id="posts-wrapper" className="flex w-full flex-col gap-4">
+                    <h2 className="text-xl font-bold">Sobre mim</h2>
+                    <span>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Enim praesentium est hic eaque consectetur ducimus
+                        repellendus porro minus illum culpa ratione quos
+                        provident unde, dicta vero excepturi dolores totam
+                        corrupti?
+                    </span>
+                </div>
+
+                <div id="posts-wrapper" className="flex w-full flex-col gap-4">
+                    <h2 className="text-xl font-bold">Skills</h2>
+                    <div className="flex flex-wrap gap-1">
+                        {skills.map((skill) => {
+                            return <Badge key={skill}>{skill}</Badge>;
+                        })}
+                    </div>
+                </div>
+
+                <div id="posts-wrapper" className="flex w-full flex-col gap-4">
+                    <h2 className="text-xl font-bold">Experiências</h2>
+
+                    <Accordion type="single" collapsible>
+                        {experiencies.map((experiencie, index) => {
+                            return (
+                                <AccordionItem
+                                    value={index.toString()}
+                                    key={index}
+                                >
+                                    <AccordionTrigger className="flex justify-between">
+                                        <div>
+                                            <h3 className="text-md">
+                                                {experiencie.enterprise}
+                                            </h3>
+                                            <span className="text-xs font-normal">
+                                                {experiencie.role}
+                                            </span>
+                                        </div>
+                                        <div className="text-sm font-normal">
+                                            <span>
+                                                {experiencie.monthFrom}{' '}
+                                                {experiencie.yearFrom}
+                                            </span>
+                                            <span> - </span>
+                                            <span>
+                                                {experiencie.monthTo}{' '}
+                                                {experiencie.yearTo}
+                                            </span>
+                                        </div>
+                                    </AccordionTrigger>
+                                    <AccordionContent>
+                                        {experiencie.description}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            );
+                        })}
+                    </Accordion>
+                </div>
+                {
+                    ////// POSTS
+                }
+                {/* {posts && (
           <div id="posts-wrapper" className="flex flex-col gap-4 w-full">
             <h2 className="text-xl font-bold">Últimas Publicações</h2>
 
             <ListiningPost posts={posts} />
           </div>
-        )}
-      </main>
-    </RevealFx>
-  );
+        )} */}
+
+                <div id="posts-wrapper" className="flex w-full flex-col gap-4">
+                    <h2 className="text-xl font-bold">Trabalhos</h2>
+                    <div className='flex flex-col gap-6'>
+                        {works.length !== 0 &&
+                            works.map((work, index) => {
+                                return (
+                                    <a key={index} href={work.link} target='_blank' className='border-b py-4'>
+                                        <div className="flex flex-col gap-2">
+                                            <h3 className="text-md font-bold">
+                                                {work.name}
+                                            </h3>
+                                            <span className="text-sm font-normal">
+                                                {work.description}
+                                            </span>
+                                            <div className="flex flex-wrap gap-2">
+                                                {work.tags.map((tag, index) => {
+                                                    return (
+                                                        <Badge key={index}>
+                                                            {tag}
+                                                        </Badge>
+                                                    );
+                                                })}
+                                            </div>
+                                        </div>
+                                    </a>
+                                );
+                            })}
+                    </div>
+                </div>
+            </main>
+        </RevealFx>
+    );
 }
---
+++ apps/web/src/content/index.ts
@@ -0,0 +1,121 @@
+export const experiencies = [
+    {
+        enterprise: 'Autônomo',
+        role: 'Full Stack Developer',
+        monthFrom: 'Jan',
+        yearFrom: '2024',
+        monthTo: ' ',
+        yearTo: 'Atuando',
+        description:
+            'Atuo como desenvolvedor Full Stack, entregando sites mais completos, robustos e seguro. Além do desenvolvimentos de sistemas também para outras empresas.',
+    },
+    {
+        enterprise: 'Group Imobi',
+        role: 'Designer Pleno',
+        monthFrom: 'Jun',
+        yearFrom: '2024',
+        monthTo: 'Out',
+        yearTo: '2024',
+        description:
+            'Desenvolvimento de posts, landing pages e materiais impressos, com participação na criação de estratégias de marketing e otimização de processos. Responsável também pela orientação, treinamento e recrutamento de novos designers.',
+    },
+
+    {
+        enterprise: 'STZ Digital',
+        role: 'Sócio Proprietário',
+        monthFrom: 'Set',
+        yearFrom: '2020',
+        monthTo: 'Mai',
+        yearTo: '2024',
+        description:
+            'Como sócio proprietário, tinha participação em todos o setores, inteligência comercial, vendas, design, marketing, desenvolvimento web, e etc. Junto com mais um sócio que veio junto comigo nessa jornada.',
+    },
+    {
+        enterprise: 'Group Imobi',
+        role: 'Designer Junior',
+        monthFrom: 'Jun',
+        yearFrom: '2023',
+        monthTo: 'Nov',
+        yearTo: '2023',
+        description:
+            'Desenvolvimento de posts, landing pages e materiais impressos, além de colaboração em estratégias de marketing e otimização de processos.',
+    },
+];
+
+export const skills = [
+    'NodeJs',
+    'React',
+    'Next',
+    'TypeScript',
+    'Postgre',
+    'Docker',
+    'Jwt',
+    'SuperTest',
+    'Jest',
+    'Prisma',
+    'Fastify',
+    'Express',
+    'Zod',
+    'Github',
+    'TailwindCSS',
+    'Storybook',
+    'Monólitos',
+    'Microsserviços',
+    'Multi-Tenant',
+];
+
+export const works = [
+    {
+        name: 'My Dev Portfólio 👜',
+        link: 'https://github.com/jeanpj12/My-Dev-Portfolio',
+        description:
+            'My Dev Portfólio é um portfólio para desenvolvedores que exibe skills, experiências e projetos. Além disso, integra um blog com um editor de texto personalizado, permitindo que o autor crie e gerencie posts de forma intuitiva.',
+        tags: [
+            'NodeJs',
+            'React',
+            'Next',
+            'TypeScript',
+            'Postgre',
+            'Docker',
+            'Jwt',
+            'Fastify',
+            'Zod',
+            'TailwindCSS',
+            'Prisma',
+        ],
+    },
+    {
+        name: 'TaskManager API 🚀',
+        link: 'https://github.com/jeanpj12/taskManager',
+        description:
+            'API para gestão de equipes e tarefas, com controle de administradores, membros, históricos de atividades e priorização de tasks.',
+        tags: [
+            'NodeJs',
+            'TypeScript',
+            'Postgre',
+            'Docker',
+            'Jwt',
+            'Express',
+            'Zod',
+            'Prisma',
+            'JWT',
+        ],
+    },
+    {
+        name: 'GGLog API 🚚',
+        link: 'https://github.com/jeanpj12/My-Dev-Portfolio',
+        description:
+            'API para gerenciamento de entregas e logística, com sistema de rastreamento, logs de status e autenticação de usuários.',
+        tags: [
+            'NodeJs',
+            'TypeScript',
+            'Postgre',
+            'Docker',
+            'Jwt',
+            'Express',
+            'Zod',
+            'Prisma',
+            'JWT',
+        ],
+    },
+];
